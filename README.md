b84a7e4c9b2b18cd1c0fd82b42c71870238d6ef2
KwonOhBin <kwonohbin1226@gmail.com>
2016-08-04 15:01:06 +0900

    react^15.3.0, react-native^0.30.0 supported

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-native-video-stream
+
+# Info
+This library based [react-native-pili](https://github.com/buhe/react-native-pili)
+
+# License
+MIT
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by buhe on 16/4/28.
+ */
+var ReactNative = {
+  // Components
+  Streaming: require('./Streaming'),
+  Player:require('./Player')
+};
+
+module.exports = ReactNative;
+//
+//var { requireNativeComponent, PropTypes, View } = require('react-native');
+//
+//var iface = {
+//      name:'DEMOJS',
+//      propTypes: {
+//        ...View.propTypes,
+//      str: PropTypes.string,
+//    },
+//    };
+//
+//module.exports = requireNativeComponent('DEMO', iface);
